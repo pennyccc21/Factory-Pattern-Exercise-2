83e067eae11958066afe0630e1831d9a8d2fac50
Peter luque <Peterluque21@gmail.com>
2022-10-14 18:51:49 -0400

    commit

---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    internal class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+             new Product(){ Name ="PS4", Price = 200},
+              new Product() { Name = "Guitar", Price = 1000},
+              new Product() { Name="Blender", Price = 50},
+              new Product () { Name="Kite", Price =25},
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading mongo data access");
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am now saving data from mongo");
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace FactoryPatternExercise2
+﻿
+using FactoryPatternExercise2;
+
+Console.WriteLine("What database would you like to work with");
+Console.WriteLine("Type: sql");
+Console.WriteLine("Type: mongo");
+Console.WriteLine("Type: list");
+
+var userInput = Console.ReadLine();
+
+IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+var products = db.LoadData();
+db.SaveData();
+
+foreach (var product in products)
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    
+    Console.Write($"Name: {item.Name} Price:{item.Price}");
 }
